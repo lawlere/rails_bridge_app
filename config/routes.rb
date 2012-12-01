39e39bdd64134bab80c0b8ed4c7aee77194e16f5
Emma Lawler <emily.lawler@colorado.edu>
2012-12-01 15:43:15 -0700

    clean up

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Suggestotron::Application.routes.draw do
 
   resources :topics
 
+  root :to => 'topics#index'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
